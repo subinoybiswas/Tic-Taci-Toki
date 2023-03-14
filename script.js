b837348a2d95d6910df93e96d64d1283cc738e3d
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-03-14 20:34:19 +0530

    Looks quite decent

---
+++ script.js
@@ -0,0 +1,11 @@
+btn1 = Document.getElementById("1");
+btn2 = Document.getElementById("2");
+btn3 = Document.getElementById("3");
+btn4 = Document.getElementById("4");
+btn5 = Document.getElementById("5");
+btn6 = Document.getElementById("6");
+btn7 = Document.getElementById("7");
+btn8 = Document.getElementById("8");
+btn9 = Document.getElementById("9");
+
+btn1.onclick(innerHTML)
