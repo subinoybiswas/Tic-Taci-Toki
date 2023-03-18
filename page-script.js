23268fcdc52c44436037e1f4cd03fe266330d544
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-03-18 12:16:00 +0530

    Added Redirect

---
+++ page-script.js
@@ -0,0 +1,7 @@
+var options = document.getElementsByClassName("op");
+var op1 = options[0];
+var op2 = options[1];
+var op3 = options[2];
+op1.onclick = () => {
+  window.location.href = "index.html";
+};
