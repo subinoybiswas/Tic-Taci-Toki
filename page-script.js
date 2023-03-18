1bf0bb83feb85ba956c67031d978a24150195ede
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-03-18 22:48:43 +0530

    Fixed ISSUE WITH NAVIGATION BUTTONS

--- page-script.js
+++ page-script.js
@@ -2,11 +2,11 @@ var options = document.getElementsByClassName("op");
 var op1 = options[0];
 var op2 = options[1];
 var op3 = options[2];
-op2.onclick = () => {
-  window.location.href = "index.html";
-};
 op1.onclick = () => {
   window.location.href = "undergoing.html";
+};
+op2.onclick = () => {
+  window.location.href = "game.html";
 };
 op3.onclick = () => {
   window.location.href = "undergoing.html";
